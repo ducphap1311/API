44edc6d58d68cc8c683ca111efebeaa270517999
ho duc phap <hophap1311@gmail.com>
2023-10-30 23:43:36 +0700

    first commit

--- controllers/User.js
+++ controllers/User.js
@@ -2,41 +2,48 @@ const User = require("../model/User");
 
 const register = async (req, res) => {
     try {
-        const { username, email, password } = req.body
+        const { username, email, password } = req.body;
         if (!username || !email || !password) {
             return res
                 .status(400)
-                .json({ msg: "Please provide necessary informations" });
+                .json({ msg: "Please provide necessary information" });
         }
         const user = await User.create(req.body);
-        const token = user.createJWT()
-        res.status(200).json({ msg: "user created", token, username: user.username });
-        
+        const token = user.createJWT();
+        res.status(200).json({
+            msg: "user created",
+            token,
+            username: user.username,
+        });
     } catch (error) {
-        return res.status(500).json({msg: "Invalid email & password"})
+        return res
+            .status(500)
+            .json({ msg: "Something went wrong, try later!" });
     }
 };
 
 const login = async (req, res) => {
-    const {email, password} = req.body
-    if(!email || !password){
-        return res.status(400).json({msg: "Please provide necessary informations"})
+    const { email, password } = req.body;
+    if (!email || !password) {
+        return res
+            .status(400)
+            .json({ msg: "Please provide necessary informations" });
     }
-    const user = await User.findOne({email: email})
-    if(!user){
-        return res.status(400).json({msg: "Invalid email & password"})
+    const user = await User.findOne({ email: email });
+    if (!user) {
+        return res.status(400).json({ msg: "Invalid email!" });
     }
     //compare password
-    const isPasswordMatch = await user.comparePassword(password)
-    if(!isPasswordMatch){
-        return res.status(400).json({msg: "password is incorect"})
+    const isPasswordMatch = await user.comparePassword(password);
+    if (!isPasswordMatch) {
+        return res.status(400).json({ msg: "Invalid password!" });
     }
-    const token = user.createJWT()
-    res.status(200).json({msg: 'user found', token, username: user.username})
+    const token = user.createJWT();
+    res.status(200).json({ msg: "user found", token, username: user.username });
 };
 
 const dashboard = async (req, res) => {
-    res.status(200).json({msg: "success"})
-}
+    res.status(200).json({ msg: "success" });
+};
 
 module.exports = { register, login, dashboard };
